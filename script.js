9d3d27303e49aa4fa72f99515c2831d88007f452
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-02-24 12:19:07 -0300

    Adiciona as bolas que receberao as cores

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const rgbText = document.querySelector('#rgb-color');
+const sectionBalls = document.querySelector('#balls');
 
 // As funções genereteNumber e genereteColor foram inspiradas na função do site: https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript
 function genereteNumber() {
@@ -13,3 +14,12 @@ function contentText() {
   rgbText.innerHTML = genereteNumber();
 }
 contentText();
+
+function createBalls() {
+  for (let index = 0; index < 6; index += 1) {
+    let ball = document.createElement('div');
+    ball.className = 'ball';
+    sectionBalls.appendChild(ball);
+  }
+}
+createBalls();
