883f8ea8d35f2234c0ab56888e72f213ff44d06a
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-03-02 15:14:20 -0300

    Adiciona um placar que incremente 3 pontos para cada acerto no jogo

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const rgbText = document.querySelector('#rgb-color');
-const ball = document.querySelectorAll('.ball');
+const balls = document.querySelector('#balls');
 const answer = document.querySelector('#answer');
 const resetGame = document.querySelector('#reset-game');
+const score = document.querySelector('#score');
 const array = [];
+let points = 0;
+score.innerHTML = `Placar: ${points}`;
 
 // A função genereteNumber foi inspirada na função do site: https://wallacemaxters.com.br/blog/2021/02/20/como-gerar-cores-aleatorias-no-javascript
 function genereteNumber() {
@@ -12,10 +15,20 @@ function genereteNumber() {
   return `(${r}, ${g}, ${b})`;
 }
 
+function createBalls() {
+  for (let index = 0; index < 6; index += 1) {
+    const ball = document.createElement('div');
+    ball.className = 'ball';
+    balls.appendChild(ball);
+  }
+}
+createBalls();
+
 function colorBalls() {
-  for (let index = 0; index < ball.length; index += 1) {
-    ball[index].style.backgroundColor = `rgb${genereteNumber()}`;
-    array.push(ball[index].style.backgroundColor);
+  const balles = document.querySelectorAll('.ball');
+  for (let index = 0; index < balles.length; index += 1) {
+    balles[index].style.backgroundColor = `rgb${genereteNumber()}`;
+    array.push(balles[index].style.backgroundColor);
   }
 }
 colorBalls();
@@ -26,24 +39,35 @@ function colorNumber() {
 }
 colorNumber();
 
-function changeParagraph() {
-  if (this.style.backgroundColor !== `rgb${rgbText.innerHTML}`) {
-    answer.innerHTML = '';
-    answer.innerHTML = 'Errou! Tente novamente!';
-  } else {
+function changeParagraph(event) {
+  if (event.target.style.backgroundColor === `rgb${rgbText.innerHTML}`) {
     answer.innerHTML = '';
     answer.innerHTML = 'Acertou!';
+    score.innerHTML = '';
+    points += 3;
+    score.innerHTML = `Placar: ${points}`;
+  } else {
+    answer.innerHTML = '';
+    answer.innerHTML = 'Errou! Tente novamente!';
   }
 }
 
 function clickBall() {
-  for (let index = 0; index < ball.length; index += 1) {
-    ball[index].addEventListener('click', changeParagraph);
+  const balles = document.querySelectorAll('.ball');
+  for (let index = 0; index < balles.length; index += 1) {
+    balles[index].addEventListener('click', changeParagraph);
   }
 }
 clickBall();
 
 function refresh() {
-  document.location.reload();
+  array.splice(0, array.length);
+  rgbText.innerHTML = '';
+  balls.innerHTML = '';
+  answer.innerHTML = 'Escolha uma cor';
+  createBalls();
+  colorBalls();
+  colorNumber();
+  clickBall();
 }
 resetGame.addEventListener('click', refresh);
